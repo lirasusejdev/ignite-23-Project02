e0dc8c55ca0f29d8bc9554eb76d613f52502bc68
lirasusejdev <lis.amaral@grupoboticario.com.br>
2023-07-04 12:09:24 -0300

    add nova configuração de theme

--- 02-ignite-timer/src/components/Button.styles.ts
+++ 02-ignite-timer/src/components/Button.styles.ts
@@ -5,18 +5,20 @@ interface ButtonContainerProps {
   variant: ButtonVariant
 }
 const buttonVariants = {
-  primary: 'purple',
+  primary: '#8257e6',
   secondary: 'orange',
   danger: 'red',
   success: 'green'
 }
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-width: 100px,
-height: 40px,
-
-${props => {
-  return css`
-    background-color: ${buttonVariants[props.variant]};
-  `
-}}
+  border-radius: '4px';
+  border: 0;
+  margin: '8px';
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+  /* ${props => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
+  }} */
 `
---
+++ 02-ignite-timer/src/styles/themes/default.ts
@@ -0,0 +1,5 @@
+export const defaultTheme = {
+  primary: '#8257e6',
+  secondary: 'orange',
+  white: '#fff'
+}
