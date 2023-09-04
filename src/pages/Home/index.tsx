0c2b1b05109d070a22024aaf7a9b058fdbeba95d
lirasusejdev <lis.amaral@grupoboticario.com.br>
2023-09-04 11:28:09 -0300

    interromper ciclo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Play } from 'phosphor-react'
+import { HandPalm, Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
@@ -10,7 +10,8 @@ import {
   Separator,
   StartCountdownButton,
   TaskInput,
-  MinutesAmoutInput
+  MinutesAmoutInput,
+  StopCountdownButton
 } from './styles'
 import { differenceInSeconds } from 'date-fns'
 import internal from 'stream'
@@ -48,6 +49,7 @@ interface Cycle {
   minutesAmount: number
   isActive: boolean
   startDate: Date
+  interruptedDate?: Date
 }
 
 export function Home() {
@@ -97,6 +99,19 @@ export function Home() {
       reset()
   }
 
+  function handleInterruptCycle() {
+    setCycles(
+      cycles.map(cycle => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() }
+        } else {
+          return cycle
+        }
+      })
+    )
+    setActiveCycleId(null)
+  }
+
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
 
@@ -117,6 +132,8 @@ export function Home() {
   const task = watch('task')
   const isSubmitDisabled = !task
 
+  console.log(cycles)
+
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
@@ -126,6 +143,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para seu projeto"
+            disabled={!!activeCycle}
             {...register('task')}
           />
 
@@ -144,6 +162,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            disabled={!!activeCycle}
             {...register('minutesAmount', { valueAsNumber: true })}
           />
 
@@ -158,10 +177,17 @@ export function Home() {
           <span>{seconds[1]}</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
-          <Play size={24} />
-          Começar
-        </StartCountdownButton>
+        {activeCycle ? (
+          <StopCountdownButton onClick={handleInterruptCycle} type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
       </form>
     </HomeContainer>
   )
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -83,7 +83,7 @@ export const Separator = styled.div`
   justify-content: center;
 `
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -94,15 +94,23 @@ export const StartCountdownButton = styled.button`
   gap: 0.5rem;
   font-weight: bold;
   cursor: pointer;
-  background: ${props => props.theme['green-500']};
   color: ${props => props.theme['gray-100']};
-
-  &:not(:disabled):hover {
-    background: ${props => props.theme['green-700']};
-  }
 
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
+  }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${props => props.theme['green-500']};
+  &:not(:disabled):hover {
+    background: ${props => props.theme['green-700']};
+  }
+`
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${props => props.theme['red-500']};
+  &:not(:disabled):hover {
+    background: ${props => props.theme['red-700']};
   }
 `
